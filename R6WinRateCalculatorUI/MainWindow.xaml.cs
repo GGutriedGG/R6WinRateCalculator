c8d48d1f9b15a5fbf097710d589a6c0c13851dbd
GGutriedGG <66772062+GGutriedGG@users.noreply.github.com>
2021-01-08 17:42:01 +0100

    Added UI and JSON manifest
    Added the source code of the UI and the calculator and added the json manifest of the Overwolf Application, which bridges game data from R6s to the UI.

---
+++ R6WinRateCalculatorUI/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+using System.IO;
+using System.Diagnostics;
+
+namespace R6WinRateCalculatorUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            void onMatchStart(object sender, RoutedEventArgs e) {
+                WaitingLabel.Visibility = Visibility.Hidden;
+                WaitingCanvas.Visibility = Visibility.Hidden;
+                loading.Visibility = Visibility.Visible;
+                System.Diagnostics.Process.Start("CMD.exe", "/C py -m pip install requests");
+                System.Diagnostics.Process.Start("CMD.exe", @"/C py res\calculator.py");
+                Thread.Sleep(5000);
+                loading.Visibility = Visibility.Hidden;
+                var players = new List<string>{};
+                players.Add(System.IO.File.ReadLines("./res/data.txt").Take(1).First());
+                players.Add(System.IO.File.ReadLines("./res/data.txt").Skip(1).Take(1).First());
+                players.Add(System.IO.File.ReadLines("./res/data.txt").Skip(2).Take(1).First());
+                players.Add(System.IO.File.ReadLines("./res/data.txt").Skip(3).Take(1).First());
+                players.Add(System.IO.File.ReadLines("./res/data.txt").Skip(4).Take(1).First());
+                players.Add(System.IO.File.ReadLines("./res/data.txt").Skip(5).Take(1).First());
+                players.Add(System.IO.File.ReadLines("./res/data.txt").Skip(6).Take(1).First());
+                players.Add(System.IO.File.ReadLines("./res/data.txt").Skip(7).Take(1).First());
+                players.Add(System.IO.File.ReadLines("./res/data.txt").Skip(8).Take(1).First());
+                players.Add(System.IO.File.ReadLines("./res/data.txt").Skip(9).Take(1).First());
+                string reliability = System.IO.File.ReadLines("./res/data.txt").Skip(10).Take(1).First();
+                string blueWinChance = System.IO.File.ReadLines("./res/data.txt").Skip(11).Take(1).First();
+                Player1BlueName.Content = players.ElementAt(0);
+                Player2BlueName.Content = players.ElementAt(1);
+                Player3BlueName.Content = players.ElementAt(2);
+                Player4BlueName.Content = players.ElementAt(3);
+                Player5BlueName.Content = players.ElementAt(4);
+                Player1OrangeName.Content = players.ElementAt(5);
+                Player2OrangeName.Content = players.ElementAt(6);
+                Player3OrangeName.Content = players.ElementAt(7);
+                Player4OrangeName.Content = players.ElementAt(8);
+                Player5OrangeName.Content = players.ElementAt(9);
+                BlueBar.Width = float.Parse(blueWinChance) * 3.76;
+                blueWinChanceLabel.Content = float.Parse(blueWinChance) + "%";
+                reliabilityLabel.Content = reliabilityLabel.Content + reliability;
+            }
+            startmatch.Click += onMatchStart;
+        }
+    }
+}
---
+++ R6WinRateCalculatorUI/bin/Debug/res/calculator.py
@@ -0,0 +1,72 @@
+﻿import requests
+
+# Example stats for debugging
+blueTeam = [["GGutriedGG.77"], ["ItsMrJellyfish"], ["MeatyMarley"], ["invedetbubble21"], ["Dark_Acquit77"]]
+orangeTeam = [["Spoid.GODSENT"], ["GoodBoy.oG"], ["Ddot."], ["RazaH.Liquid"], ["Benjamaster2k"]]
+
+# Gets player stats from API
+for i in blueTeam:
+    url = 'https://api2.r6stats.com/public-api/stats/' + i[0] + '/pc/generic'
+    stats = requests.get(url, headers={'Authorization': 'Bearer b47d5f65-5f4a-4afc-bb35-551cfa21e8e8'})
+    statsdict = dict(stats.json())
+    progressiondict = dict(statsdict["progression"])
+    statsdict2 = dict(statsdict["stats"])
+    generaldict = dict(statsdict2["general"])
+    i.append(generaldict["wl"] * 50)
+    i.append(progressiondict["level"])
+    print(i[1], i[2])
+for i in orangeTeam:
+    url = 'https://api2.r6stats.com/public-api/stats/' + i[0] + '/pc/generic'
+    stats = requests.get(url, headers={'Authorization': 'Bearer b47d5f65-5f4a-4afc-bb35-551cfa21e8e8'})
+    statsdict = dict(stats.json())
+    progressiondict = dict(statsdict["progression"])
+    statsdict2 = dict(statsdict["stats"])
+    generaldict = dict(statsdict2["general"])
+    i.append(generaldict["wl"] * 50)
+    i.append(progressiondict["level"])
+    print(i[1], i[2])
+
+# Calculates reliability
+blueReliability = 100
+orangeReliability = 100
+for i in blueTeam:
+    if i[2] <= 25:
+        blueReliability -= 20
+for i in orangeTeam:
+    if i[2] <= 25:
+        orangeReliability -= 20
+
+# Calculates winrate
+blueRating = 0
+orangeRating = 0
+for i in blueTeam:
+    blueRating += i[1]
+for i in orangeTeam:
+    orangeRating += i[1]
+blueRating /= 5
+blueWinChance = blueRating / orangeRating * 50
+orangeWinChance = 100 - blueWinChance
+
+# Prints winrate and reliability
+print(str(round(blueRating / 100, 2)), " vs ", str(round(orangeRating / 100, 2)))
+print("Blue has a win chance of " + str(round(blueWinChance, 1)) + "%")
+print("Orange has a win chance of " + str(round(orangeWinChance, 1)) + "%")
+if blueReliability <= orangeReliability:
+    print("Reliability: " + str(blueReliability))
+    realiability = blueReliability
+else:
+    print("Reliability: " + str(orangeReliability))
+    reliability = orangeRealiability
+
+# Writes data to text file to transfer to interface
+data = open("res\data.txt","w")
+blueWinChance = round(blueWinChance, 0)
+for i in blueTeam:
+    data.write(i[0])
+    data.write("\n")
+for i in orangeTeam:
+    data.write(i[0])
+    data.write("\n")
+data.write(str(realiability))
+data.write("\n")
+data.write(str(blueWinChance))
